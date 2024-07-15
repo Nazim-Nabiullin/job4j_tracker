7038da8fb80bc15dbfebd30ef9bddecc0f446ea1
Pit Bul <Nazim55555@yandex.ru>
2024-07-15 06:58:43 +0300

    1.3. Вызов метода с аргументами. [#166086 #504750]

--- src/main/java/ru/job4j/oop/JukeboxWithSwitch.java
+++ src/main/java/ru/job4j/oop/JukeboxWithSwitch.java
@@ -1,21 +1,22 @@
 package ru.job4j.oop;
 
 public class JukeboxWithSwitch {
-    public String musicWithSwitchMatcher(int position) {
+    public void music(int position) {
         String result = switch (position) {
             case 1 -> "Пусть бегут неуклюже";
             case 2 -> "Спокойной ночи";
             default -> "Песня не найдена";
         };
-        return result;
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
         JukeboxWithSwitch jukeboxWithSwitch = new JukeboxWithSwitch();
-        System.out.println(jukeboxWithSwitch.musicWithSwitchMatcher(1));
-        System.out.println(jukeboxWithSwitch.musicWithSwitchMatcher(2));
-        System.out.println(jukeboxWithSwitch.musicWithSwitchMatcher(0));
-        System.out.println(jukeboxWithSwitch.musicWithSwitchMatcher(3));
-        System.out.println(jukeboxWithSwitch.musicWithSwitchMatcher(-3));
+        jukeboxWithSwitch.music(1);
+        jukeboxWithSwitch.music(2);
+        jukeboxWithSwitch.music(0);
+        jukeboxWithSwitch.music(3);
+        jukeboxWithSwitch.music(-3);
+
     }
 }
