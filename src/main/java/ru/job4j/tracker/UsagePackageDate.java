971f49304a39e7f90b6d677b69dcc28fc06a721b
Pit Bul <Nazim55555@yandex.ru>
2024-07-16 10:53:06 +0300

    8. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/UsagePackageDate.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class UsagePackageDate {
+    public static void main(String[] args) {
+        LocalDate currentDate = LocalDate.now();
+        System.out.println("Текущая дата: " + currentDate);
+        System.out.println();
+        LocalTime currentTime = LocalTime.now();
+        System.out.println("Текущее время: " + currentTime);
+        System.out.println();
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        System.out.println("Текущие дата и время до форматирования: " + currentDateTime);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String currentDateTimeFormat = currentDateTime.format(formatter);
+        System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+
+    }
+}
