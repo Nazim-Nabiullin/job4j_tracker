0a7879c20ae1204a71eaf4d1317994237ae4301b
Pit Bul <Nazim55555@yandex.ru>
2024-07-16 16:37:55 +0300

    0. Что такое инкапсуляция. [#3818]

--- src/main/java/ru/job4j/example/ExampleTest.java
+++ src/main/java/ru/job4j/example/ExampleTest.java
@@ -10,8 +10,8 @@ public class ExampleTest {
         String perimetr = String.format("%.1f", 4 * a);
         System.out.println(perimetr);
         String area = String.format("%.1f", 2 * Math.PI * r);
-        System.out.println(area); // длина окружности
+        System.out.println(area);
         String square = String.format("%.1f", Math.PI * Math.pow(r, 2));
-        System.out.println(square); // площадь круга
+        System.out.println(square);
     }
 }
