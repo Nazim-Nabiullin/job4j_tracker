38a3fc053bc16c2fd24e060266e9cc0cda38a100
Pit Bul <Nazim55555@yandex.ru>
2024-07-17 16:45:40 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -39,4 +39,5 @@ public class Item {
     public LocalDateTime getCreated() {
         return created;
     }
+
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,30 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] resultItems = new Item[size];
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item != null) {
+                resultItems[index] = item;
+            }
+        }
+        return Arrays.copyOf(resultItems, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] resultItems = new Item[size];
+        int count = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (key.equals(item.getName())) {
+                resultItems[index] = item;
+                count++;
+            }
+        }
+        return Arrays.copyOf(resultItems, count);
     }
 
     public Item findById(int id) {
