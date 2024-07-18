09fb3ddf2cecba497277e72b700651e47a6a3471
Pit Bul <Nazim55555@yandex.ru>
2024-07-18 13:22:49 +0300

    8. Что такое валидация? [#246864]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -47,19 +47,20 @@ public class Tracker {
     }
 
     public boolean replace(int id, Item item) {
-        boolean trueReplace = false;
         int index = indexOf(id);
-        if (index != -1) {
+        boolean result = index != -1;
+        if (result) {
             item.setId(index);
             items[index].setName(item.getName());
-            trueReplace = true;
+            result = true;
         }
-        return trueReplace;
+        return result;
     }
 
     public void delete(int id) {
         int index = indexOf(id);
-        if (index != -1) {
+        boolean result = index != -1;
+        if (result) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
