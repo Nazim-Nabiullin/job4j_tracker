bae042410587f02c3a19ce3f5101a12e76edfe67
Pit Bul <Nazim55555@yandex.ru>
2024-07-18 11:40:53 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -75,7 +75,7 @@ public class TrackerTest {
     }
 
     @Test
-    public void whenReplaceItemNotSuccessful() {
+    public void whenReplaceItemIsNotSuccessful() {
         Tracker tracker = new Tracker();
         Item item = new Item("Bug");
         tracker.add(item);
@@ -83,5 +83,24 @@ public class TrackerTest {
         boolean result = tracker.replace(1000, updateItem);
         assertThat(tracker.findById(item.getId()).getName()).isEqualTo("Bug");
         assertThat(result).isFalse();
+    }
+
+    @Test
+    public void whenDeleteItemIsSuccessful() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Bug");
+        tracker.add(item);
+        int id = item.getId();
+        tracker.delete(id);
+        assertThat(tracker.findById(id)).isNull();
+    }
+
+    @Test
+    public void whenDeleteItemIsNotSuccessful() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Bug");
+        tracker.add(item);
+        tracker.delete(1000);
+        assertThat(tracker.findById(item.getId()).getName()).isEqualTo("Bug");
     }
 }
